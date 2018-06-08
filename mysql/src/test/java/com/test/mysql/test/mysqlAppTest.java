316ebf3a90716f85322feacf9115be7cf0456aca
shmilyou <1159171637@qq.com>
2018-06-08 11:00:12 +0800

    mysql module use druid

--- mysql/src/test/java/com/test/mysql/test/mysqlAppTest.java
+++ mysql/src/test/java/com/test/mysql/test/mysqlAppTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,8 +20,12 @@ public class mysqlAppTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
     public void test(){
         List<User> users = userRepository.findAll();
+        System.out.println("====检查使用的连接池===="+dataSource.getClass());
     }
 }
